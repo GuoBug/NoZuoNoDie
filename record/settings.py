2e72b2502fbd21e2478cb6fdbdadcd8443081b56
GuoBug <zhanggggfd@gmail.com>
2014-02-22 19:08:13 +0800

    for site setting

--- record/settings.py
+++ record/settings.py
@@ -22,7 +22,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'zhanggggfd',
         'PASSWORD': 'gq830016',
-        'HOST': '172.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }

6f13aed2f42699fb616fd45f9a2233ff5f9999d2
GuoBug <zhanggggfd@gmail.com>
2014-02-22 19:16:01 +0800

    for site setting

--- record/settings.py
+++ record/settings.py
@@ -71,7 +71,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/home/zhanggggfd/NoZuoNoDie/static/'
+STATIC_URL = 'http://guoqiang.us1.rpvhost.net:4211/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (

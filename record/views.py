ea24351653dd48f7a0931a6ab3372354bcd9f915
GuoBug <zhanggggfd@gmail.com>
2014-02-23 15:04:40 +0800

    database test

--- record/views.py
+++ record/views.py
@@ -10,5 +10,5 @@ def Contact(request):
 
 def HomePage(request):
 	allBlog = Blog.objects.values('title','date','context','category')
-	print allBlog['title']
+	print type(allBlog)
 	return render_to_response('home.html',{'bloginfo': allBlog})
